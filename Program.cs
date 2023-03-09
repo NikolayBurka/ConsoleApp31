06e579fe7a2d846d24f9185188c324fe21584bf8
nikitailinskii <anastasiabalickaa435@gmail.com>
2023-03-09 12:14:51 +0200

    fd

--- Program.cs
+++ Program.cs
@@ -22,18 +22,23 @@ namespace ConsoleApp31
             //{
             //    Console.WriteLine($"array[{i}]={array[i]}");
             //}
-            Console.Write("n=");
-            byte n = byte.Parse(Console.ReadLine());
-            double[] array = new double[n];
-            Random ind = new Random();
-            for (int i = 0; i < array.Length; i++)
+            //Console.Write("n=");
+            //byte n = byte.Parse(Console.ReadLine());
+            //double[] array = new double[n];
+            //Random ind = new Random();
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = -50 + 100 * ind.NextDouble();
+            //    Console.WriteLine($"array[{i}]={i}");
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine($"array[0] ={array[0]}\narray[n-1}] ={array[n-1]}");
+
+            int[] array = new int[] { 37, 0, 50, 46, 0, 13 };
+            foreach (int element in array)
             {
-                array[i] = -50 + 100 * ind.NextDouble();
-                Console.WriteLine($"array[{i}]={i}");
+                Console.Write($"{element}\n");
             }
-            Console.WriteLine();
-            Console.WriteLine($"array[0] ={array[0]}\narray[n-1}] ={array[n-1]}");
-            1
         }
     }
 }
