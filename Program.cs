5fb91da11776f2261f7434ec38fb978b9da3ff11
nikitailinskii <anastasiabalickaa435@gmail.com>
2023-03-09 12:08:38 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] array = new int[6];
+            //for (int i=0; i<6; i++)
+            //{
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+
+            //int[] array = new int[12];
+            //Random ind = new Random();
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    Console.WriteLine($"array[{i}]");
+            //    array[i] = ind.Next(163, 190);
+            //}
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+            //}
+            Console.Write("n=");
+            byte n = byte.Parse(Console.ReadLine());
+            double[] array = new double[n];
+            Random ind = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = -50 + 100 * ind.NextDouble();
+                Console.WriteLine($"array[{i}]={i}");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"array[0] ={array[0]}\narray[{n-1}] ={array[n-1]}");
+        }
+    }
+}
